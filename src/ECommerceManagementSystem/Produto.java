1378ca8b02c9f60ade1dda0072473066809590ef
Lourenco Madoro <lourencomadoro912@gmail.com>
2026-08-05 18:58:21 +0200

    comecando um novo desafio, criei classe Produto com seus atributos

---
+++ src/ECommerceManagementSystem/Produto.java
@@ -0,0 +1,22 @@
+package ECommerceManagementSystem;
+
+public class Produto {
+    private String name;
+    private double preco;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public double getPreco(){
+        return preco;
+    }
+
+    public void  setPreco(double preco){
+        this.preco=preco;
+    }
+}
