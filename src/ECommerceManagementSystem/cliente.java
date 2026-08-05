ef0bf536969811b5c9da523eccd97bbccd2225e0
Lourenco Madoro <lourencomadoro912@gmail.com>
2026-08-05 19:16:47 +0200

    criei classe clente com atributos basicos

---
+++ src/ECommerceManagementSystem/cliente.java
@@ -0,0 +1,20 @@
+package ECommerceManagementSystem;
+
+public class cliente {
+    private String nome;
+    private int idade;
+
+    public  cliente(String nome, int idade){
+        this.nome=nome;
+        this.idade=idade;
+    }
+
+    //Nome: get and set
+    public  String getNome(){return  nome;}
+    public  void setNome(String nome){this.nome=nome;}
+
+    //Idade: get and set
+    public  int getIdade(){return  idade;}
+    public void setIdade(int idade){this.idade=idade;}
+
+}
