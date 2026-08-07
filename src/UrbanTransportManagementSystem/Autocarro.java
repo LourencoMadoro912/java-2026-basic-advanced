7a3a64e23fccef21d30a9539c63377dc6fa6571b
Lourenco Madoro <lourencomadoro912@gmail.com>
2026-08-07 10:32:40 +0200

    criei classe Autocarro e Taxi, extendi a classe veiculo

---
+++ src/UrbanTransportManagementSystem/Autocarro.java
@@ -0,0 +1,12 @@
+package UrbanTransportManagementSystem;
+
+public class Autocarro extends Veiculo{
+    public Autocarro(String placa, int capacidade){
+        super(placa,capacidade);
+    }
+
+    @Override
+    public String toString(){
+        return super.toString();
+    }
+}
---
+++ src/UrbanTransportManagementSystem/Taxi.java
@@ -0,0 +1,11 @@
+package UrbanTransportManagementSystem;
+
+public class Taxi extends Veiculo{
+    public Taxi(String placa, int capacidade){
+        super(placa,capacidade);
+    }
+
+    public String toString(){
+        return super.toString();
+    }
+}
